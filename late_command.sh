d8d4846ab2e9ee5c41774d5d579b6b7d30730c3b
Max Goldman <maxg@meet.mit.edu>
2010-07-09 19:13:00 +0300

    Serve preseed and late_command from GitHub

---
+++ late_command.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# sudoers
+echo 'Defaults mail_always' >> /etc/sudoers
+echo '%lab-admins ALL= ALL' >> /etc/sudoers
+
+# Configuration files
+GIT=$(mktemp -d)
+git clone --no-checkout https://github.com/meet/config-fatlab-ubuntu-lucid.git "$GIT"
+mv "$GIT"/.git /
+(cd / && git checkout --force)
+rm -rf "$GIT"
+
+# NFS mount points
+mkdir -p /opt/MEET
+mkdir -p /etc/opt/MEET
